c9170b64ab57115daff2193dc851f65094d4cc64
Joel Huggett <joel@forestry.io>
2020-11-17 18:22:23 -0400

    styling homepage

--- pages/testimonials.tsx
+++ pages/testimonials.tsx
@@ -1,9 +1,17 @@
 import Head from 'next/head'
+import styled from 'styled-components'
 
 export default function Home() {
   return (
     <div>
+      <Header>
+        
+      </Header>
      
     </div>
   )
 }
+
+const Header = styled.div`
+  height: 25em;
+`
